01148fbfb2452fa5c74b12ac53119d34113a1daa
Vedansh Mehra <vedanshofficial@gmail.com>
2023-09-21 02:54:32 +0530

    implemented 404 page not found page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from "react"
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import Home1 from "./components/Home1"
-import NavbarCart from "./components/NavbarCart"
-import Checkout from "./components/Checkout"
-import Login from "./components/Login"
-import Cart from "./components/CartPage"
-import BulkOrder from "./components/BulkOrder"
-import ContactUs from "./components/ContactUs"
-import Payment from "./components/payment"
-import { CartProvider } from "./components/CartContext"
-import Dashboard from "./components/Dashboard"
-import Terms from "./components/Terms&Conditions"
-import Policy from "./components/Privacypolicy"
-import BulkCheckout from "./components/BulkCheckout"
-import { Thankyou } from "./components/Thankyou"
-import HowItWorks from "./components/HowItWorks"
-import Register from "./components/Register"
-import ScrollToTop from "./components/scroll"
-import ForgetPassword from "./components/ForgetPassword"
-import ResetPassword from "./components/ResetPassword"
-import ProtectedRoute from "./shared-components/protected-route"
-import Reset from "./components/reset"
-import { AuthContext } from "./context/auth-context"
-import { useCookies } from "react-cookie"
-import Invoice from "./components/Invoice"
-import Email from "./components/Emailtemplateimages"
-import ShowItem from "./components/showItem"
-import NotFound from "./components/NotFound"
-import { PaymentFailed } from "./components/PaymentFailed"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Home1 from "./components/Home1";
+import NavbarCart from "./components/NavbarCart";
+import Checkout from "./components/Checkout";
+import Login from "./components/Login";
+import Cart from "./components/CartPage";
+import BulkOrder from "./components/BulkOrder";
+import ContactUs from "./components/ContactUs";
+import Payment from "./components/payment";
+import { CartProvider } from "./components/CartContext";
+import Dashboard from "./components/Dashboard";
+import Terms from "./components/Terms&Conditions";
+import Policy from "./components/Privacypolicy";
+import BulkCheckout from "./components/BulkCheckout";
+import { Thankyou } from "./components/Thankyou";
+import HowItWorks from "./components/HowItWorks";
+import Register from "./components/Register";
+import ScrollToTop from "./components/scroll";
+import ForgetPassword from "./components/ForgetPassword";
+import ResetPassword from "./components/ResetPassword";
+import ProtectedRoute from "./shared-components/protected-route";
+import Reset from "./components/reset";
+import { AuthContext } from "./context/auth-context";
+import { useCookies } from "react-cookie";
+import Invoice from "./components/Invoice";
+import Email from "./components/Emailtemplateimages";
+import ShowItem from "./components/showItem";
+import NotFound from "./components/NotFound";
+import { PaymentFailed } from "./components/PaymentFailed";
 
 function App() {
-  const [cartItems, setCartItems] = useState([])
-  const [selectedProvider, setSelectedProvider] = useState("All")
-  const [selectedPrice, setSelectedPrice] = useState("low")
-  const [user, setUser] = useState(null)
-  const [cookies] = useCookies(["pfAuthToken"])
+  const [cartItems, setCartItems] = useState([]);
+  const [selectedProvider, setSelectedProvider] = useState("All");
+  const [selectedPrice, setSelectedPrice] = useState("low");
+  const [user, setUser] = useState(null);
+  const [cookies] = useCookies(["pfAuthToken"]);
 
   const handleAddToCart = (item) => {
-    setCartItems([...cartItems, item])
-  }
+    setCartItems([...cartItems, item]);
+  };
 
   useEffect(() => {
     if (cookies?.pfAuthToken) {
-      setUser(JSON?.parse(localStorage?.getItem("user")))
+      setUser(JSON?.parse(localStorage?.getItem("user")));
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -184,15 +184,15 @@ function App() {
               <Routes>
                 <Route path="/how-it-works" element={<HowItWorks />} />
               </Routes>
-              {/* <Routes>
+              <Routes>
                 <Route path="*" element={<NotFound />} />
-              </Routes> */}
+              </Routes>
             </ScrollToTop>
           </Router>
         </CartProvider>
       </AuthContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
