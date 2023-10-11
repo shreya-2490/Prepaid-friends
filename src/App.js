f49cc4cdacc15e58ce1d119d2269cd78be5c0cd3
shreya072001 <gsmile.india201@gmail.com>
2023-10-11 22:50:10 +0530

    fixed buyorder invoice page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home1 from "./components/Home1";
-import NavbarCart from "./components/NavbarCart";
-import Login from "./components/Login";
-import BulkOrder from "./components/BulkOrder";
-import ContactUs from "./components/ContactUs";
-import Payment from "./components/payment";
-import { CartProvider } from "./components/CartContext";
-import Dashboard from "./components/Dashboard";
-import Terms from "./components/Terms&Conditions";
-import Policy from "./components/Privacypolicy";
-import BulkCheckout from "./components/BulkCheckout";
-import { Thankyou } from "./components/Thankyou";
-import HowItWorks from "./components/HowItWorks";
-import Register from "./components/Register";
-import ScrollToTop from "./components/scroll";
-import ForgetPassword from "./components/ForgetPassword";
-import ResetPassword from "./components/ResetPassword";
-import ProtectedRoute from "./shared-components/protected-route";
-import Reset from "./components/reset";
-import { AuthContext } from "./context/auth-context";
-import { useCookies } from "react-cookie";
-import Invoice from "./components/Invoice";
-import Email from "./components/Emailtemplateimages";
-import ShowItem from "./components/showItem";
-import NotFound from "./components/NotFound";
-import { PaymentFailed } from "./components/PaymentFailed";
+import React, { useEffect, useState } from "react"
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home1 from "./components/Home1"
+import NavbarCart from "./components/NavbarCart"
+import Login from "./components/Login"
+import BulkOrder from "./components/BulkOrder"
+import ContactUs from "./components/ContactUs"
+import Payment from "./components/payment"
+import { CartProvider } from "./components/CartContext"
+import Dashboard from "./components/Dashboard"
+import Terms from "./components/Terms&Conditions"
+import Policy from "./components/Privacypolicy"
+import BulkCheckout from "./components/BulkCheckout"
+import { Thankyou } from "./components/Thankyou"
+import HowItWorks from "./components/HowItWorks"
+import Register from "./components/Register"
+import ScrollToTop from "./components/scroll"
+import ForgetPassword from "./components/ForgetPassword"
+import ResetPassword from "./components/ResetPassword"
+import ProtectedRoute from "./shared-components/protected-route"
+import Reset from "./components/reset"
+import { AuthContext } from "./context/auth-context"
+import { useCookies } from "react-cookie"
+import Invoice from "./components/Invoice"
+import Email from "./components/Emailtemplateimages"
+import ShowItem from "./components/showItem"
+import NotFound from "./components/NotFound"
+import { PaymentFailed } from "./components/PaymentFailed"
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const [selectedProvider, setSelectedProvider] = useState("All");
-  const [selectedPrice, setSelectedPrice] = useState("low");
-  const [user, setUser] = useState(null);
-  const [cookies] = useCookies(["pfAuthToken"]);
+  const [cartItems, setCartItems] = useState([])
+  const [selectedProvider, setSelectedProvider] = useState("All")
+  const [selectedPrice, setSelectedPrice] = useState("low")
+  const [user, setUser] = useState(null)
+  const [cookies] = useCookies(["pfAuthToken"])
 
   const handleAddToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
+    setCartItems([...cartItems, item])
+  }
 
   useEffect(() => {
     if (cookies?.pfAuthToken) {
-      setUser(JSON?.parse(localStorage?.getItem("user")));
+      setUser(JSON?.parse(localStorage?.getItem("user")))
     }
-  }, []);
+  }, [])
 
   return (
     <div className="App">
@@ -70,14 +70,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<ForgetPassword />} />
                 <Route path="/register" element={<Register />} />
-                <Route
-                  path="/invoice"
-                  element={
-                    <ProtectedRoute>
-                      <Invoice />
-                    </ProtectedRoute>
-                  }
-                />
+                <Route path="/invoice" element={<Invoice />} />
                 <Route
                   path="/reset-password/:stoken"
                   element={<ResetPassword />}
@@ -142,7 +135,7 @@ function App() {
         </CartProvider>
       </AuthContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
